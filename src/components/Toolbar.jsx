3a0f18a9542c49f06a0b105477c1d3b5d1da3333
Daniil Alabuzhev <90332154+Kuroky79@users.noreply.github.com>
2023-02-17 01:05:53 +0300

    rect

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -4,13 +4,14 @@ import toolStore from "../store/toolStore";
 import Brush from "../Tools/Brush";
 import canvas from "./Canvas";
 import canvasState from "../store/CanvasState";
+import Rect from "../Tools/Rect";
 
 
 const Toolbar = () => {
     return (
         <div className="toolbar">
             <button className="toolbar__btn brush" onClick={()=>toolStore.setTool(new Brush((canvasState.canvas)))}></button>
-            <button className="toolbar__btn rect"></button>
+            <button className="toolbar__btn rect" onClick={()=>toolStore.setTool(new Rect((canvasState.canvas)))}></button>
             <button className="toolbar__btn circle"></button>
             <button className="toolbar__btn eraser"></button>
             <button className="toolbar__btn line"></button>
